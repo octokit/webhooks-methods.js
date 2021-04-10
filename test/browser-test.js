634fd125e1f5e53f745519b252d41d86e72d0f9e
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-04-10 15:16:27 -0700

    feat: initial version (#1)

---
+++ test/browser-test.js
@@ -0,0 +1,37 @@
+const { strictEqual } = require("assert");
+
+const { readFile } = require("fs").promises;
+const puppeteer = require("puppeteer");
+
+runTests();
+
+async function runTests() {
+  const script = await readFile("pkg/dist-web/index.js", "utf-8");
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("file:///");
+
+  await page.addScriptTag({
+    content: script.replace("export { sign, verify };", ""),
+  });
+
+  const [signature, verified] = await page.evaluate(async function () {
+    const signature = await sign("secret", "data");
+    console.log(signature);
+
+    const verified = await verify("secret", "data", signature);
+    console.log(verified);
+
+    return [signature, verified];
+  });
+
+  strictEqual(
+    signature,
+    "1b2c16b75bd2a870c114153ccda5bcfca63314bc722fa160d690de133ccbb9db"
+  );
+  strictEqual(verified, true);
+
+  await browser.close();
+
+  console.log("All tests passed.");
+}
---
+++ test/sign.test.ts
@@ -0,0 +1,122 @@
+import { sign } from "../src";
+
+const eventPayload = {
+  foo: "bar",
+};
+const secret = "mysecret";
+
+describe("sign", () => {
+  it("is a function", () => {
+    expect(sign).toBeInstanceOf(Function);
+  });
+
+  it("sign.VERSION is set", () => {
+    expect(sign.VERSION).toEqual("0.0.0-development");
+  });
+
+  test("throws without options throws", async () => {
+    // @ts-expect-error
+    await expect(() => sign()).rejects.toThrow(
+      "[@octokit/webhooks-methods] secret & payload required for sign()"
+    );
+  });
+
+  test("throws without secret", async () => {
+    // @ts-ignore
+    await expect(() => sign(undefined, eventPayload)).rejects.toThrow(
+      "[@octokit/webhooks-methods] secret & payload required for sign()"
+    );
+  });
+
+  test("throws without eventPayload", async () => {
+    // @ts-expect-error
+    await expect(() => sign(secret)).rejects.toThrow(
+      "[@octokit/webhooks-methods] secret & payload required for sign()"
+    );
+  });
+
+  test("sign({secret, algorithm}) throws with invalid algorithm", async () => {
+    await expect(() =>
+      // @ts-expect-error
+      sign({ secret, algorithm: "sha2" }, eventPayload)
+    ).rejects.toThrow(
+      "[@octokit/webhooks] Algorithm sha2 is not supported. Must be  'sha1' or 'sha256'"
+    );
+  });
+
+  describe("with eventPayload as object", () => {
+    describe("returns expected sha1 signature", () => {
+      test("sign(secret, eventPayload)", async () => {
+        const signature = await sign(secret, eventPayload);
+        expect(signature).toBe(
+          "sha256=4864d2759938a15468b5df9ade20bf161da9b4f737ea61794142f3484236bda3"
+        );
+      });
+
+      test("sign({secret}, eventPayload)", async () => {
+        const signature = await sign({ secret }, eventPayload);
+        expect(signature).toBe(
+          "sha256=4864d2759938a15468b5df9ade20bf161da9b4f737ea61794142f3484236bda3"
+        );
+      });
+
+      test("sign({secret, algorithm: 'sha1'}, eventPayload)", async () => {
+        const signature = await sign(
+          { secret, algorithm: "sha1" },
+          eventPayload
+        );
+        expect(signature).toBe("sha1=d03207e4b030cf234e3447bac4d93add4c6643d8");
+      });
+    });
+
+    describe("returns expected sha256 signature", () => {
+      test("sign({secret, algorithm}, eventPayload)", async () => {
+        const signature = await sign(
+          { secret, algorithm: "sha256" },
+          eventPayload
+        );
+        expect(signature).toBe(
+          "sha256=4864d2759938a15468b5df9ade20bf161da9b4f737ea61794142f3484236bda3"
+        );
+      });
+    });
+  });
+
+  describe("with eventPayload as string", () => {
+    describe("returns expected sha1 signature", () => {
+      test("sign(secret, eventPayload)", async () => {
+        const signature = await sign(secret, JSON.stringify(eventPayload));
+        expect(signature).toBe(
+          "sha256=4864d2759938a15468b5df9ade20bf161da9b4f737ea61794142f3484236bda3"
+        );
+      });
+
+      test("sign({secret}, eventPayload)", async () => {
+        const signature = await sign({ secret }, JSON.stringify(eventPayload));
+        expect(signature).toBe(
+          "sha256=4864d2759938a15468b5df9ade20bf161da9b4f737ea61794142f3484236bda3"
+        );
+      });
+
+      test("sign({secret, algorithm: 'sha1'}, eventPayload)", async () => {
+        const signature = await sign(
+          { secret, algorithm: "sha1" },
+          JSON.stringify(eventPayload)
+        );
+        expect(signature).toBe("sha1=d03207e4b030cf234e3447bac4d93add4c6643d8");
+      });
+    });
+
+    describe("returns expected sha256 signature", () => {
+      test("sign({secret, algorithm: 'sha256'}, eventPayload)", async () => {
+        const signature = await sign(
+          { secret, algorithm: "sha256" },
+          JSON.stringify(eventPayload)
+        );
+        expect(signature).toBe(
+          "sha256=4864d2759938a15468b5df9ade20bf161da9b4f737ea61794142f3484236bda3"
+        );
+      });
+    });
+  });
+});
