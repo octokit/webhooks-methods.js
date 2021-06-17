a69f98a6f0b1fb7b1c28a98f8bd553f72cda3e37
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-06-17 12:09:03 -0700

    test: only accept payload as string

--- test/verify.test.ts
+++ test/verify.test.ts
@@ -1,12 +1,18 @@
 import { verify } from "../src";
 
-const eventPayload = {
-  foo: "bar",
-};
+function toNormalizedJsonString(payload: object) {
+  // GitHub sends its JSON with an indentation of 2 spaces and a line break at the end
+  const payloadString = JSON.stringify(payload, null, 2) + "\n";
+  return payloadString.replace(/[^\\]\\u[\da-f]{4}/g, (s) => {
+    return s.substr(0, 3) + s.substr(3).toUpperCase();
+  });
+}
+
+const eventPayload = toNormalizedJsonString({ foo: "bar" });
 const secret = "mysecret";
-const signatureSHA1 = "sha1=d03207e4b030cf234e3447bac4d93add4c6643d8";
+const signatureSHA1 = "sha1=640c0ea7402a3f74e1767338fa2dba243b1f2d9c";
 const signatureSHA256 =
-  "sha256=4864d2759938a15468b5df9ade20bf161da9b4f737ea61794142f3484236bda3";
+  "sha256=e3eccac34c43c7dc1cbb905488b1b81347fcc700a7b025697a9d07862256023f";
 
 describe("verify", () => {
   it("is a function", () => {
@@ -64,26 +70,26 @@ describe("verify", () => {
     // https://github.com/octokit/webhooks.js/issues/71
     const signatureMatchesLowerCaseSequence = await verify(
       "development",
-      {
+      toNormalizedJsonString({
         foo: "Foo\n\u001b[34mbar: ♥♥♥♥♥♥♥♥\nthis-is-lost\u001b[0m\u001b[2K",
-      },
-      "sha1=7316ec5e7866e42e4aba4af550d21a5f036f949d"
+      }),
+      "sha1=82a91c5aacc9cdc2eea893bc828bd03d218df79c"
     );
     expect(signatureMatchesLowerCaseSequence).toBe(true);
     const signatureMatchesUpperCaseSequence = await verify(
       "development",
-      {
+      toNormalizedJsonString({
         foo: "Foo\n\u001B[34mbar: ♥♥♥♥♥♥♥♥\nthis-is-lost\u001B[0m\u001B[2K",
-      },
-      "sha1=7316ec5e7866e42e4aba4af550d21a5f036f949d"
+      }),
+      "sha1=82a91c5aacc9cdc2eea893bc828bd03d218df79c"
     );
     expect(signatureMatchesUpperCaseSequence).toBe(true);
     const signatureMatchesEscapedSequence = await verify(
       "development",
-      {
+      toNormalizedJsonString({
         foo: "\\u001b",
-      },
-      "sha1=2c440a176f4cb84c8c921dfee882d594c2465097"
+      }),
+      "sha1=bdae4705bdd827d026bb227817ca025b5b3a6756"
     );
     expect(signatureMatchesEscapedSequence).toBe(true);
   });
@@ -111,26 +117,26 @@ describe("verify", () => {
     // https://github.com/octokit/webhooks.js/issues/71
     const signatureMatchesLowerCaseSequence = await verify(
       "development",
-      {
+      toNormalizedJsonString({
         foo: "Foo\n\u001b[34mbar: ♥♥♥♥♥♥♥♥\nthis-is-lost\u001b[0m\u001b[2K",
-      },
-      "sha256=afecc3caa27548bb90d51a50384cb2868b9a3327b4ad6a01c9bd4ed0f8b0b12c"
+      }),
+      "sha256=9dacf9003316b09be07df56d86a2d0d6872e42a1e6c72c3bad9ff915a7c5603e"
     );
     expect(signatureMatchesLowerCaseSequence).toBe(true);
     const signatureMatchesUpperCaseSequence = await verify(
       "development",
-      {
+      toNormalizedJsonString({
         foo: "Foo\n\u001B[34mbar: ♥♥♥♥♥♥♥♥\nthis-is-lost\u001B[0m\u001B[2K",
-      },
-      "sha256=afecc3caa27548bb90d51a50384cb2868b9a3327b4ad6a01c9bd4ed0f8b0b12c"
+      }),
+      "sha256=9dacf9003316b09be07df56d86a2d0d6872e42a1e6c72c3bad9ff915a7c5603e"
     );
     expect(signatureMatchesUpperCaseSequence).toBe(true);
     const signatureMatchesEscapedSequence = await verify(
       "development",
-      {
+      toNormalizedJsonString({
         foo: "\\u001b",
-      },
-      "sha256=6f8326efbacfbd04e870cea25b5652e635be8c9807f2fd5348ef60753c9e96ed"
+      }),
+      "sha256=87316067e2011fae39998b18c46a14d83b3e7c3ffdd88fb2ee5afb7d11288e60"
     );
     expect(signatureMatchesEscapedSequence).toBe(true);
   });
