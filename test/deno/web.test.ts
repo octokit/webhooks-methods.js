86f68eacd27188a2b9511c07381b8ca385edfe0b
Mark Fulton <mfulton26@gmail.com>
2022-09-10 17:23:14 -0500

    test: Deno tests (#81)

---
+++ test/deno/web.test.ts
@@ -0,0 +1,18 @@
+import { sign, verify } from "../../src/web.ts";
+
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("sign", async () => {
+  const actual = await sign("secret", "data");
+  const expected =
+    "1b2c16b75bd2a870c114153ccda5bcfca63314bc722fa160d690de133ccbb9db";
+  assertEquals(actual, expected);
+});
+
+Deno.test("verify", async () => {
+  const signature =
+    "1b2c16b75bd2a870c114153ccda5bcfca63314bc722fa160d690de133ccbb9db";
+  const actual = await verify("secret", "data", signature);
+  const expected = true;
+  assertEquals(actual, expected);
+});
