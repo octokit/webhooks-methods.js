0e76989b9d8d1172daba5e98ba798c853faff63f
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2023-06-09 19:24:56 -0400

    build: fix typo

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -31,7 +31,7 @@ async function main() {
       outdir: "pkg/dist-node",
       bundle: true,
       platform: "node",
-      target: "node18,
+      target: "node18",
       format: "cjs",
       ...sharedOptions,
     }),
