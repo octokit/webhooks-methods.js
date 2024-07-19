9e1bd974182fa9a290d3522ba7c5ac739ef4733f
Aras Abbasi <aras.abbasi@googlemail.com>
2024-07-19 14:24:09 +0200

    chore: use vitest, improve ci (#283)
    * chore: use vitest, improve ci
    
    * upgrade deno tests
    
    * run the scripts
    
    * try to fix ci
    
    * fix deno, add coverage module (d'oh')
    
    * remove glob as esbuild comes with its own glob parser
    
    * add lint
    
    * make lint mandatory, update deps
    
    * pin to 22.4.1
    
    * will renaming summary back to test fix it?

--- test/sign.test.ts
+++ test/sign.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from "vitest";
 import { sign } from "../src/index.ts";
 
 const eventPayload = {
@@ -14,21 +15,21 @@ describe("sign", () => {
     expect(sign.VERSION).toEqual("0.0.0-development");
   });
 
-  test("throws without options throws", async () => {
+  it("throws without options throws", async () => {
     // @ts-expect-error
     await expect(() => sign()).rejects.toThrow(
       "[@octokit/webhooks-methods] secret & payload required for sign()",
     );
   });
 
-  test("throws without secret", async () => {
+  it("throws without secret", async () => {
     // @ts-ignore
     await expect(() => sign(undefined, eventPayload)).rejects.toThrow(
       "[@octokit/webhooks-methods] secret & payload required for sign()",
     );
   });
 
-  test("throws without eventPayload", async () => {
+  it("throws without eventPayload", async () => {
     // @ts-expect-error
     await expect(() => sign(secret)).rejects.toThrow(
       "[@octokit/webhooks-methods] secret & payload required for sign()",
@@ -37,7 +38,7 @@ describe("sign", () => {
 
   describe("with eventPayload as string", () => {
     describe("returns expected sha256 signature", () => {
-      test("sign(secret, eventPayload)", async () => {
+      it("sign(secret, eventPayload)", async () => {
         const signature = await sign(secret, JSON.stringify(eventPayload));
         expect(signature).toBe(
           "sha256=4864d2759938a15468b5df9ade20bf161da9b4f737ea61794142f3484236bda3",
@@ -46,7 +47,7 @@ describe("sign", () => {
     });
   });
 
-  test("throws with eventPayload as object", () => {
+  it("throws with eventPayload as object", () => {
     // @ts-expect-error
     expect(() => sign(secret, eventPayload)).rejects.toThrow(
       "[@octokit/webhooks-methods] payload must be a string",
