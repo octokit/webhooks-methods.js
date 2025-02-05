34653d880947594ba1a2d6ed7cdc151d722a57bf
wolfy1339 <webmaster@wolfy1339.com>
2025-02-05 10:25:07 -0500

    fix: add `verifyWithFallback()` function in the web entrpoint

--- src/web.ts
+++ src/web.ts
@@ -79,3 +79,26 @@ export async function verify(
     enc.encode(eventPayload),
   );
 }
+export async function verifyWithFallback(
+  secret: string,
+  payload: string,
+  signature: string,
+  additionalSecrets: undefined | string[],
+): Promise<any> {
+  const firstPass = await verify(secret, payload, signature);
+
+  if (firstPass) {
+    return true;
+  }
+
+  if (additionalSecrets !== undefined) {
+    for (const s of additionalSecrets) {
+      const v: boolean = await verify(s, payload, signature);
+      if (v) {
+        return v;
+      }
+    }
+  }
+
+  return false;
+}
