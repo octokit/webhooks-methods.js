86288b30c54df25173bb066972767c30592fdd03
Aras Abbasi <aras.abbasi@googlemail.com>
2025-07-07 00:59:48 +0200

    remove redundant check

--- src/node/timing-safe-equal.ts
+++ src/node/timing-safe-equal.ts
@@ -1,8 +1,3 @@
 import { timingSafeEqual as cryptoTimingSafeEqual } from "node:crypto";
 
-export function timingSafeEqual(a: Uint8Array, b: Uint8Array): boolean {
-  if (a.length !== b.length) {
-    return false;
-  }
-  return cryptoTimingSafeEqual(a, b);
-}
+export const timingSafeEqual = cryptoTimingSafeEqual;
--- src/web/timing-safe-equal.ts
+++ src/web/timing-safe-equal.ts
@@ -2,9 +2,6 @@
 // https://stackoverflow.com/a/31096242/206879
 // https://en.wikipedia.org/wiki/Timing_attack
 export function timingSafeEqual(a: Uint8Array, b: Uint8Array): boolean {
-  if (a.length !== b.length) {
-    return false;
-  }
   var len = a.length;
   var out = 0;
   var i = -1;
