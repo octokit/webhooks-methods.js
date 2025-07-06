71c0019313cc4069a3e791056f9fcc72f1ae6da2
Aras Abbasi <aras.abbasi@googlemail.com>
2025-07-06 19:39:04 +0200

    remove unnecessary type checks

--- src/common/hmac-sha256.ts
+++ src/common/hmac-sha256.ts
@@ -13,18 +13,6 @@ export async function hmacSha256({
   data,
   key,
 }: HmacSha256Options): Promise<Uint8Array> {
-  if (!key || !data) {
-    throw new TypeError(
-      "[@octokit/webhooks-methods] key & data required for hmacSha256()",
-    );
-  }
-
-  if (!(key instanceof Uint8Array) || !(data instanceof Uint8Array)) {
-    throw new TypeError(
-      "[@octokit/webhooks-methods] key & data must be Uint8Array",
-    );
-  }
-
   if (key.length > blockSize) {
     key = await sha256(key);
   } else if (key.length < blockSize) {
