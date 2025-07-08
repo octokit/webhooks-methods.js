d11a68dfbb0af25ae20bcf8f10aebad1ada630cd
Aras Abbasi <aras.abbasi@googlemail.com>
2025-07-08 15:12:46 +0200

    use again webcrypto for web

--- src/web/hmac-sha256.ts
+++ src/web/hmac-sha256.ts
@@ -1,33 +1,22 @@
-const blockSize = 64;
-
 export async function hmacSha256(
   key: Uint8Array,
   data: Uint8Array,
 ): Promise<Uint8Array> {
-  const keyLength = key.length;
-  if (keyLength > blockSize) {
-    key = new Uint8Array(await crypto.subtle.digest("SHA-256", key), 0, 32);
-  }
-
-  const iKeyPad = new Uint8Array(blockSize + data.length);
-  const oKeyPad = new Uint8Array(blockSize + 32);
-
-  for (let i = 0; i < keyLength; i++) {
-    iKeyPad[i] = key[i] ^ 0x36;
-    oKeyPad[i] = key[i] ^ 0x5c;
-  }
-
-  for (let i = keyLength; i < blockSize; i++) {
-    iKeyPad[i] = 0x36;
-    oKeyPad[i] = 0x5c;
-  }
-
-  iKeyPad.set(data, blockSize);
-  const innerHash = new Uint8Array(
-    await crypto.subtle.digest("SHA-256", iKeyPad),
-    0,
-    32,
+  const importedKey = await crypto.subtle.importKey(
+    "raw", // raw format of the key - should be Uint8Array
+    key, // the key to import
+    {
+      // algorithm details
+      name: "HMAC",
+      hash: { name: "SHA-256" },
+    },
+    false, // export = false
+    ["sign", "verify"], // what this key can do
   );
-  oKeyPad.set(innerHash, blockSize);
-  return new Uint8Array(await crypto.subtle.digest("SHA-256", oKeyPad), 0, 32);
+  const signature = await crypto.subtle.sign(
+    "HMAC",
+    importedKey, // the key to use for signing
+    data,
+  );
+  return new Uint8Array(signature);
 }
